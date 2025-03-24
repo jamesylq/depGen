ff386ee2605717e15e912ea0518df4ed4d798af2
jamesylq <jamesylq@gmail.com>
2025-03-24 18:55:30 +0800

    Added saving of Profiles & Events

--- app/src/main/java/com/example/depgen/SettingsPage.kt
+++ app/src/main/java/com/example/depgen/SettingsPage.kt
@@ -1,2 +1,39 @@
 package com.example.depgen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingsPage() {
+    Scaffold (
+        topBar = {
+            DefaultTopAppBar()
+        }
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            if (Global.idx == 1) {
+                CardButton("Clear Data", {
+                    //TODO: Alert
+                    clear()
+                })
+            } else {
+                CardButton("Delete Account", {
+                    //TODO: Alert
+                    Global.profileList.removeAt(Global.idx)
+                    switchProfile(LOGGED_OUT)
+                    navController.navigate("Login")
+                })
+            }
+        }
+    }
+}
